e000bc7b8ad278ca55276cc8d0d11e5f352b199a
Isadora Dias <diasfisadora@gmail.com>
2025-11-14 16:28:04 -0300

    feat: update project structure and improve configuration

--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,12 @@ const routes = Router()
 
 const mockGroups = [{id: 1, name: 'treino A', description: 'peito e triceps'}, {id: 2, name: 'treino B', description: 'costas e biceps'}]
 
-routes.get('/grupos-treino', (req, res)=>{
+routes.get('/workout-groups', (req, res)=>{
     console.log('Listando grupos de treino')
     return res.status(200).json(mockGroups)
 })
 
-routes.get('/grupos-treino/:id', (req, res)=>{
+routes.get('/workout-groups/:id', (req, res)=>{
     const {id} = req.params
 
     const group = mockGroups.find(group => group.id === Number(id))
